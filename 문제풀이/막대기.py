d50738937f8e0a16cbbd99477227a4224fc4d8fa
황도영 <ghkdehdud96@naver.com>
2023-07-08 14:49:47 +0900

    2023-07-08 문제풀이 : 막대기, S5

---
+++ 문제풀이/막대기.py
@@ -0,0 +1,25 @@
+# https://www.acmicpc.net/problem/18870
+import sys
+
+sys.stdin = open("sample.txt", 'r')
+
+stick = [64, 32, 16, 8, 4, 2, 1]
+
+n = int(input())
+
+cnt = 0
+st = 0
+now = 0
+flag = True
+while flag:
+    a = stick[now]
+    if st + a > n:
+        now += 1
+    else:
+        st += stick[now]
+        cnt += 1
+
+    if st == n:
+        flag = False
+
+print(cnt)
