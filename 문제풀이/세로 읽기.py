f70dc7a4f8db98f14638438cecef4ad61be52796
황도영 <ghkdehdud96@naver.com>
2023-07-13 15:44:38 +0900

    2023-07-13 문제풀이 : 세로읽기, B1

---
+++ 문제풀이/세로 읽기.py
@@ -0,0 +1,10 @@
+import sys
+
+sys.stdin = open("sample.txt", 'r')
+
+words = [input() for i in range(5)]
+
+for c in range(15):
+    for r in range(5):
+        if c < len(words[r]):
+            print(words[r][c], end='')
